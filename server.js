e8c91900ce784e5041936c1d02387226de3203ca
Bilal H <hussain.bilal000786@gmail.com>
2017-12-28 22:17:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to find the server log');
+    }
+  })
+  next();
+})
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+//   //next();
+// })
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs',{
+    pageTitle: 'Bilal-h.com',
+    welcomeMessage: 'Welcome to my first node App..'
+  })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+      pageTitle: 'About page Ttl'
+    });
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    ErrorMessage: 'ErrorMessage'
+  })
+})
+
+app.listen(3000,() => {
+  console.log('server has been started');
+});
